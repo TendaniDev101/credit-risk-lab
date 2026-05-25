66147490bffcfbf29bf73cd52927bdc89a16ad1f
tendanimukhithi <tendani@charge.co.za>
2026-05-25 22:41:22 +0200

    Add plotting dependencies and notebook visuals

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -6,6 +6,8 @@ cd "$SCRIPT_DIR"
 
 source .venv/bin/activate
 
+pip install -r requirements.txt
+
 export JAVA_HOME="/opt/homebrew/opt/openjdk@17/libexec/openjdk.jdk/Contents/Home"
 export PATH="/opt/homebrew/opt/openjdk@17/bin:$PATH"
 export SPARK_LOCAL_IP="127.0.0.1"
